662a11f700c4e29e296e973568e400768e51eb8c
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-03-22 19:10:23 +0530

    backend added

---
+++ backend/db.js
@@ -0,0 +1,55 @@
+// backend/db.js
+const mongoose = require('mongoose');
+
+
+mongoose.connect('')
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const acccountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId;
+        ref: 'User', //if a user is present them only assciate it with an account
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+// Create a model from the schema
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User
+};
